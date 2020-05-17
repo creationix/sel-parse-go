e0b36ca8a8d5a3d4aa426aa39bb967b97b9ad482
Tim Caswell <tim@creationix.com>
2020-05-16 23:07:04 -0500

    Use the proper selector library to integrate with

--- main.go
+++ main.go
@@ -1,33 +1,17 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
 
-	"github.com/ipld/go-ipld-schema/parser"
+	basicnode "github.com/ipld/go-ipld-prime/node/basic"
+	"github.com/ipld/go-ipld-prime/traversal/selector/builder"
+
 	"github.com/kr/pretty"
 )
 
-func parse(path string) error {
-	fi, err := os.Open(path)
-	if err != nil {
-		return err
-	}
-	defer fi.Close()
-
-	s := bufio.NewScanner(fi)
-	sch, err := parser.ParseSchema(s)
-	if err != nil {
-		return err
-	}
-
-	fmt.Printf("%# v", pretty.Formatter(sch))
-
-	return nil
-}
-
 func main() {
-	parse("selector.ipldsch")
-
+	ns := basicnode.Style__Any{}
+	ssb := builder.NewSelectorSpecBuilder(ns)
+	node := ssb.ExploreAll(ssb.Matcher()).Node()
+	fmt.Printf("%# v\n", pretty.Formatter(node))
 }
